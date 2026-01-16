13430fbe4c8b6c20598412af306ab3a786b067c5
Saurav Nepal <antoneyosaurav111@gmail.com>
2026-01-16 20:02:43 +0545

    Initial setup

--- src/migrations/1758435339178-CreateBusinessSettingDetailTable.ts
+++ src/migrations/1758435339178-CreateBusinessSettingDetailTable.ts
@@ -10,7 +10,6 @@ export class CreateBusinessSettingDetailTable1758435339178 extends MigrationUtil
     this.primary();
 
     this.foreign({ name: 'business_id', foreignTable: 'bz_business_details' });
-    this.boolean('is_private_number', false);
 
     this.json('attributes');
     this.whoColumns();
