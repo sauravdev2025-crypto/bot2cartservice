4b51f104f2247100d871d9cab523a289a22caddd
Saurav Nepal <antoneyosaurav111@gmail.com>
2026-01-16 20:05:46 +0545

    FIXED

--- src/business/entities/business.entity.ts
+++ src/business/entities/business.entity.ts
@@ -12,42 +12,6 @@ import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 @Entity('bz_business_details')
 export class BusinessEntity extends AccessBusinessEntity {
   @Column()
-  internal_id: string;
-
-  @Column()
-  internal_access_token: string;
-
-  @Column()
-  internal_number: string;
-
-  @Column()
-  verified_at: Date;
-
-  @Column()
-  expired_at: Date;
-
-  @Column()
-  default_mobile: string;
-
-  @Column()
-  phone_registered_at: Date;
-
-  @Column('json')
-  last_health_status: any;
-
-  @Column('json')
-  quality_response: any;
-
-  @Column()
-  wa_display_name: string;
-
-  @Column()
-  total_message_limit: number;
-
-  @Column()
-  total_sent: number;
-
-  @Column()
   is_partner_account: boolean;
 
   @Column()
